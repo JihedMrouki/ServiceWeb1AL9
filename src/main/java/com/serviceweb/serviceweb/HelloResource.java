aaf678e1e04b64a4f4ef5e46126ffa1b0a49cbab
Jihed Mrouki <47598290+JihedMrouki@users.noreply.github.com>
2023-06-26 00:57:26 +0100

    todo

--- src/main/java/com/serviceweb/serviceweb/HelloResource.java
+++ src/main/java/com/serviceweb/serviceweb/HelloResource.java
@@ -4,11 +4,48 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
-@Path("/hello-world")
+@Path("/employes")
 public class HelloResource {
-    @GET
-    @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
-    }
+  static List<Employe> employes = new ArrayList<Employe>();
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String addEmploye(Employe emp) {
+		
+		if(employes.add(emp))
+			return "Employe ajout�";
+		return "Employe non ajout�";
+		
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)//"application/json"
+	public List<Employe> getAllEmploye()
+	{
+		return employes;
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String updateEmploye(Employe emp) {
+		int index = employes.indexOf(emp);
+		if(index != -1) {
+			employes.set(index, emp);
+			return "employ� modifi�";
+		}
+		return "employ� non modifi�";
+	}
+	
+	@DELETE
+	@Path("{a}")
+	public boolean deleteEmploye(@PathParam("a") String cin)
+	{
+		Iterator<Employe>it = employes.iterator();
+		while(it.hasNext()) {
+			if(it.next().getCin().equals(cin));
+			it.remove();
+			return true;
+		}
+		return false;
+	}
 }
---
+++ src/main/java/com/serviceweb/serviceweb/employe.java
@@ -0,0 +1,53 @@
+package com.serviceweb.serviceweb;
+
+
+public class Employe {
+	
+	private String cin;
+	private String nom;
+	private String prenom;
+	public String getCin() {
+		return cin;
+	}
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cin == null) ? 0 : cin.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employe other = (Employe) obj;
+		if (cin == null) {
+			if (other.cin != null)
+				return false;
+		} else if (!cin.equals(other.cin))
+			return false;
+		return true;
+	}
+	
+	
+
+}
